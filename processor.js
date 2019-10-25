6ca4b1a38fd104a98f3188329c0e23c6435f30c5
hm <hide-mikami@omni.co.jp>
2019-10-25 18:50:45 +0900

    comp test

--- processor.js
+++ processor.js
@@ -37,9 +37,9 @@ let processor = {
     computeFrame: function () {
         this.ctx1.drawImage(this.video, 0, 0, window.innerWidth, window.innerHeight);
         this.ctx1.drawImage(this.img, 0, 0, window.innerWidth, window.innerHeight - 18);
-        this.ctx2.drawImage(this.movie, 1000, 1000, this.width, this.height);
+        this.ctx2.drawImage(this.movie, 0, 0, this.width, this.height);
 
-        let frame = this.ctx2.getImageData(1000, 1000, this.width, this.height);
+        let frame = this.ctx2.getImageData(0, 0, this.width, this.height);
         let l = frame.data.length / 4;
 
         for (let i = 0; i < l; i++) {
