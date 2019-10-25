0403c51f1468815d447f4ac9056df34fb12832dd
hm <hide-mikami@omni.co.jp>
2019-10-25 20:17:47 +0900

    comp test

--- processor.js
+++ processor.js
@@ -37,9 +37,9 @@ let processor = {
     computeFrame: function () {
         this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
         this.ctx1.drawImage(this.img, 0, 0, this.width, this.height);
-        this.ctx2.drawImage(this.movie, 0, 0, this.width, this.height);
+        this.ctx1.drawImage(this.movie, 0, 0, this.width, this.height);
 
-        let frame = this.ctx2.getImageData(0, 0, this.width, this.height);
+        let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
         let l = frame.data.length / 4;
 
         //クロマキー
