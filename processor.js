46ba6c168d422fd933ada9db10792bafb0bc57ed
hm <hide-mikami@omni.co.jp>
2019-10-25 20:14:06 +0900

    comp test

--- processor.js
+++ processor.js
@@ -50,7 +50,7 @@ let processor = {
             if (g > 100 && r < 100 && b < 100)
                 frame.data[i * 4 + 3] = 0;
         }
-        this.ctx2.putImageData(frame, 200, 200);
+        this.ctx2.putImageData(frame, 100, 100);
         return;
     }
   };
