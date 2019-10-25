882fde55c51a9b5b41bc623641f109e0c929d6aa
hm <hide-mikami@omni.co.jp>
2019-10-25 21:30:10 +0900

    TechTeamKai

---
+++ app.js
@@ -0,0 +1,48 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "environment" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraFrame = document.querySelector("#camera--frame")
+
+
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+        drawFrame();
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0)   
+    cameraSensor.getContext("2d").drawImage(cameraFrame, 0, 0, cameraView.videoWidth, cameraView.videoHeight)   
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+function drawFrame(){
+    const image = new Image();
+    image.src = cameraFrame.src;
+    image.onload = () => {
+      const ctx = cameraFrame.getContext("2d");
+      //ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      ctx.clearRect(0, 0);
+      ctx.drawImage(image, 0, 0);
+    };
+}
+
